bc7d22c809cf85fbcd8da6c4039d97a47ae2dc1a
inhomun <inho8063@icloud.com>
2022-07-28 15:34:54 +0900

    BOJ_3009

---
+++ practicing/Example.swift
@@ -0,0 +1,15 @@
+//
+//  Example.swift
+//  practicing
+//
+//  Created by 문인호 on 2022/07/23.
+//
+
+import Foundation
+
+func printHelloWithName(name: String) {
+    print("\(name), Hello")
+}
+
+let inputname = "hong-gil-dong"
+printHelloWithName(name: inputname)
