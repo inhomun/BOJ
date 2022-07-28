4d1c5d19d99761827b9c6500d2942f3adf8e778a
inhomun <inho8063@icloud.com>
2022-07-28 15:53:53 +0900

    BOJ1550

--- practicing/main.swift
+++ practicing/main.swift
@@ -5,25 +5,8 @@
 //  Created by 문인호 on 2022/07/14.
 //
 
-
 import Foundation
 
-var number = readLine()!.split(separator: " ").map{ Int(String($0))! }
-
-var H = number[0]
-var M = number[1]
-
-var M1 : Int! = M-45
-
-if M1 >= 0 {
-    print("\(H) \(M1!)")
-}
-
-if M1 < 0 {
-    if H-1 < 0 {
-        print("23 \(60+M1)")
-    }
-    else {
-        print("\(H-1) \(60+M1)")
-    }
-}
+let Num = readLine()!
+let decimal:Int = Int(Num,radix: 16)!
+print(decimal)
