86e615ac8c42710eb30c64b7e62104c1eb8def5b
inhomun <inho8063@icloud.com>
2022-08-01 21:27:54 +0900

    BOJ_1977

--- practicing/main.swift
+++ practicing/main.swift
@@ -5,8 +5,29 @@
 //  Created by 문인호 on 2022/07/14.
 //
 
-import Foundation
+    import Foundation
 
-let Num = readLine()!
-let decimal:Int = Int(Num,radix: 16)!
-print(decimal)
+var M = Int(readLine()!)!
+var N = Int(readLine()!)!
+var arrNum : Array<Int> = []
+var sum : Int! = 0
+for j in M ... N {
+    for i in 1...100{
+        if j == (i*i) {
+            arrNum.append(j)
+        }
+    }
+}
+
+if arrNum.count == 0 {
+    print(-1)
+}
+else {
+    for i in 0..<arrNum.count{
+       sum += arrNum[i] 
+    }
+    print(sum!)
+    print(arrNum[0])
+}
+
+
