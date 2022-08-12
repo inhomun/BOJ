425f01ba5f7a9ea66c77953031cb801c316e4222
inhomun <inho8063@icloud.com>
2022-08-12 14:31:31 +0900

    BOJ_9095

--- practicing/main.swift
+++ practicing/main.swift
@@ -1,40 +1,23 @@
-//
-//  BOJ_2753.swift
+
 //  practicing
 //
 //  Created by 문인호 on 2022/07/14.
 //
 import Foundation
+var dp = [Int](repeating: 0, count: 12)
 
-let procession = readLine()!.split(separator: " ").map{ Int($0)! }
-var n = procession[0]
-var m = procession[1]
-var arrA: [[Int]] = Array(repeating: Array(repeating:0 ,count: m), count: n)
-var arrB: [[Int]] = Array(repeating: Array(repeating:0 ,count: m), count: n)
-var array1 = [Int]()
-var array2 = [[Int]]()
-
-for i in 0..<arrA.count{
-    arrA[i] = readLine()!.components(separatedBy: " ").map{ Int(String($0))! }
-} // 생성된 이중 배열에 값 입력받는 for 문
-
-for i in 0..<arrB.count{
-    arrB[i] = readLine()!.components(separatedBy: " ").map{ Int(String($0))! }
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in 4..<12
+{
+    dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
 }
 
-for i in 0..<arrA.count{
-    for j in 0..<arrA[i].count{
-        let sum = arrA[i][j] + arrB[i][j]
-        array1.append(sum)
-    }
-    array2.append(array1)
-    array1.removeAll()
+let T = Int(readLine()!)!
+for _ in 1...T{
+    var a = Int(readLine()!)!
+    print(dp[a])
 }
 
-for i in 0..<n {
-    for j in 0..<m {
-        print(array2[i][j], terminator: " ")
-    }
-print()
-}
 
