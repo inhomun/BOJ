ddb3643afa26eaed5725e01656e07e0d8c504fbe
inhomun <inho8063@icloud.com>
2022-08-16 15:16:03 +0900

    BOJ_25372

--- practicing/main.swift
+++ practicing/main.swift
@@ -5,41 +5,13 @@
 //
 import Foundation
 
-var arr1 = Array(readLine()!)
-var arr2 = Array(readLine()!)
-
-var arr3 = [Int]()
-
-for i in 0..<8 {
-    arr3.append(Int(String(arr1[i]))!)
-    arr3.append(Int(String(arr2[i]))!)
-}
-
-var i = 0
-var j = 1
-
-while !(arr3.count == 2) {
-    var temp = [Int]()
-    
-    while !(j == arr3.count) {
-        let sum = arr3[i]+arr3[j]
-        if sum < 10 {
-            temp.append(Int(String(sum))!)
-        }
-        
-        else {
-            temp.append(Int(String(sum%10))!)
-        }
-        
-        i += 1
-        j += 1
+let N = Int(readLine()!)!
+for _ in 1...N {
+    let strVal = readLine()!
+    if strVal.count > 5 && strVal.count < 10 {
+        print("yes")
     }
-    arr3 = temp
-    i = 0
-    j = 1
+    else {
+        print("no")
+    }
 }
-
-for i in arr3 {
-    print(i, terminator: "")
-}
-
