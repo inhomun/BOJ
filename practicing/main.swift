1473e812a15417ad545f84819996f64a58be86d4
inhomun <inho8063@icloud.com>
2022-08-26 19:31:01 +0900

    BOJ_7785

--- practicing/main.swift
+++ practicing/main.swift
@@ -2,13 +2,19 @@
 //  practicing
 //
 //  Created by 문인호 on 2022/07/14.
-//
 import Foundation
 
-func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
-    var sum = 0
-    for (i, isPositive) in signs.enumerated(){
-        sum += isPositive ? absolutes[i] : -(absolutes[i])
-    }
-    return sum
+var dict = [String : Bool]()
+
+let N = Int(readLine()!)!
+
+for _ in 0..<N {
+    let input = readLine()!.split(separator: " ").map { String($0) }
+    if input[1] == "enter" {
+        dict[input[0]] = true
+    } else {
+        dict[input[0]] = nil    }
+}
+dict.keys.sorted(by: >).forEach { name in
+    print(name)
 }
