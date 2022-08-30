6b2e044edee2d0a0d2ff83902575d79c82c03e0b
inhomun <inho8063@icloud.com>
2022-08-30 12:50:17 +0900

    BOJ_1764

--- practicing/main.swift
+++ practicing/main.swift
@@ -3,18 +3,20 @@
 //
 //  Created by 문인호 on 2022/07/14.
 import Foundation
-
-var dict = [String : Bool]()
-
-let N = Int(readLine()!)!
-
+let input2 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = input2[0]
+let M = input2[1]
+var listenable = Set <String> ()
+var watchable = Set <String> ()
 for _ in 0..<N {
-    let input = readLine()!.split(separator: " ").map { String($0) }
-    if input[1] == "enter" {
-        dict[input[0]] = true
-    } else {
-        dict[input[0]] = nil    }
+    let input = readLine()!
+    listenable.insert(input)
 }
-dict.keys.sorted(by: >).forEach { name in
-    print(name)
+for _ in 0..<M {
+    let input1 = readLine()!
+    watchable.insert(input1)
 }
+let set1 = listenable.intersection(watchable)
+print(set1.count)
+set1.sorted(by: <).forEach { name in
+    print(name) }
