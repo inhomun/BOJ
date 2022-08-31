11594a65dd51585adc474b84c94061bb73401716
inhomun <inho8063@icloud.com>
2022-08-31 15:57:46 +0900

    BOJ_11650

--- practicing/main.swift
+++ practicing/main.swift
@@ -3,20 +3,15 @@
 //
 //  Created by 문인호 on 2022/07/14.
 import Foundation
-let input2 = readLine()!.split(separator: " ").map { Int(String($0))! }
-let N = input2[0]
-let M = input2[1]
-var listenable = Set <String> ()
-var watchable = Set <String> ()
-for _ in 0..<N {
-    let input = readLine()!
-    listenable.insert(input)
+let A = Int(readLine()!)!
+var xYArray: [(Int, Int)] = []
+for _ in 0..<A {
+    let xAndY = readLine()!.split(separator: " ")
+        xYArray.append((Int(xAndY[0])!, Int(xAndY[1])!))
 }
-for _ in 0..<M {
-    let input1 = readLine()!
-    watchable.insert(input1)
+xYArray.sort(by: {
+    $0.0 == $1.0 ? $0.1 < $1.1 : $0.0 < $1.0
+})
+for x in 0..<A {
+    print("\(xYArray[x].0) \(xYArray[x].1)")
 }
-let set1 = listenable.intersection(watchable)
-print(set1.count)
-set1.sorted(by: <).forEach { name in
-    print(name) }
