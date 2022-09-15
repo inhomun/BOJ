238bca00e163b652c6a605d63fc8957c93fb0963
inhomun <inho8063@icloud.com>
2022-09-15 12:31:51 +0900

    programmers_문자열_내_p와_y의_개수

--- practicing/main.swift
+++ practicing/main.swift
@@ -2,7 +2,27 @@
 //  practicing
 //
 //  Created by 문인호 on 2022/07/14.
+import Foundation
 
-func solution(_ s:String) -> Int {
-    return Int(s)!
+func solution(_ s:String) -> Bool
+{
+    var cnt1 = 0
+    var cnt2 = 0
+    for i in s {
+        if i == "p" {
+             cnt1 += 1
+        }
+        else if i == "P" {
+            cnt1 += 1
+        }
+        else if i == "y" {
+            cnt2 += 1
+        }
+        else if i == "Y" {
+            cnt2 += 1
+        }
+    }
+    var ans:Bool = (cnt1 == cnt2)
+    
+    return ans
 }
