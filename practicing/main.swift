138f37bd1b2be70bbbda1e49dcbaa27038f940a4
inhomun <inho8063@icloud.com>
2022-09-23 13:28:28 +0900

    programmers_하샤드수

--- practicing/main.swift
+++ practicing/main.swift
@@ -3,26 +3,11 @@
 //
 //  Created by 문인호 on 2022/07/14.
 import Foundation
-
-func solution(_ s:String) -> Bool
-{
-    var cnt1 = 0
-    var cnt2 = 0
-    for i in s {
-        if i == "p" {
-             cnt1 += 1
-        }
-        else if i == "P" {
-            cnt1 += 1
-        }
-        else if i == "y" {
-            cnt2 += 1
-        }
-        else if i == "Y" {
-            cnt2 += 1
-        }
-    }
-    var ans:Bool = (cnt1 == cnt2)
-    
-    return ans
+func solution(_ x:Int) -> Bool {
+  var sum = 0
+  for i in String(x) {
+    guard let number = Int(String(i)) else { break }
+    sum += number
+  }
+  return x % sum == 0
 }
