176250d7b3b08e025be7ad6ebe9c20933bec0e6a
inhomun <inho8063@icloud.com>
2022-09-28 15:36:12 +0900

    숫자 문자열과 영단어_programmers

--- practicing/main.swift
+++ practicing/main.swift
@@ -3,11 +3,17 @@
 //
 //  Created by 문인호 on 2022/07/14.
 import Foundation
-func solution(_ x:Int) -> Bool {
-  var sum = 0
-  for i in String(x) {
-    guard let number = Int(String(i)) else { break }
-    sum += number
-  }
-  return x % sum == 0
+func solution(_ s:String) -> Int {
+    var answer:String = s
+    answer = answer.replacingOccurrences(of: "zero", with: "0")
+    answer = answer.replacingOccurrences(of: "one", with: "1")
+    answer = answer.replacingOccurrences(of: "two", with: "2")
+    answer = answer.replacingOccurrences(of: "three", with: "3")
+    answer = answer.replacingOccurrences(of: "four", with: "4")
+    answer = answer.replacingOccurrences(of: "five", with: "5")
+    answer = answer.replacingOccurrences(of: "six", with: "6")
+    answer = answer.replacingOccurrences(of: "seven", with: "7")
+    answer = answer.replacingOccurrences(of: "eight", with: "8")
+    answer = answer.replacingOccurrences(of: "nine", with: "9")
+    return Int(answer)!
 }
