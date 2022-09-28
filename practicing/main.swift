486c46602018573e9b3ab0fe5803290ad1838033
inhomun <inho8063@icloud.com>
2022-09-28 15:50:34 +0900

    평균_구하기_programmers

--- practicing/main.swift
+++ practicing/main.swift
@@ -3,11 +3,6 @@
 //
 //  Created by 문인호 on 2022/07/14.
 import Foundation
-func solution(_ num:Int) -> String {
-    if (num%2==0) {
-        return "Even"
-    }
-    else {
-        return "Odd"
-    }
+func solution(_ arr:[Int]) -> Double {
+    return Double(arr.reduce(0, +))/Double((arr.count))
 }
